cbcf2f0275bec17b3034b9c1d2d270b55e2e2b72
Megan Ennis <Megan@Megans-iMac-9.local>
2019-10-25 00:58:34 +0100

    Single post styled

--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -3,6 +3,7 @@ import Axios from "axios";
 import { getSinglePost } from "../state/actionCreators";
 import { connect } from "react-redux";
 import * as actionCreators from "../state/actionCreators";
+import "./scss/singlepost.scss";
 
 export function SinglePost(props) {
   const id = props.match.params.postId;
@@ -21,15 +22,17 @@ export function SinglePost(props) {
     return <p>Loading</p>;
   }
   return (
-    <div>
-      <img src={props.post.post.imgURL} />
-      <h4>{props.post.post.title}</h4>
-      <p>{props.post.post.yield}</p>
-      <p>{props.post.post.serving}</p>
-      <p>{props.post.post.total_time}</p>
-      <p>{props.post.post.cook_time}</p>
-      <p>{props.post.post.ingredient}</p>
-      <p>{props.post.post.directions}</p>
+    <div className="single-post">
+      <div className="recipe-img">
+        <img src={props.post.post.imgURL} />
+      </div>
+      <div className="recipe-content">
+        <h2>{props.post.post.title}</h2>
+        <p> <span>Servings:</span> {props.post.post.yield}</p>
+        <p> <span>Total time required:</span> {props.post.post.total_time}</p>
+        <p> <span>Main ingredient:</span> {props.post.post.ingredient}</p>
+        <p> <span>Instructions:</span> {props.post.post.directions}</p>
+      </div>
     </div>
   );
 }
